b8d156b52313d77166292ba0f0bc24404b926c10
dTop <jroney002@gmail.com>
2024-09-20 10:36:41 -0500

    css and embedding

--- React-Portfolio/src/components/Footer.jsx
+++ React-Portfolio/src/components/Footer.jsx
@@ -1,1 +1,14 @@
-icons and links to social/github
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/fluviangumbo">
+                <FaGithubSquare />
+            </a>
+            <a href="https://www.linkedin.com/in/joel-roney-099983161/">
+                <FaLinkedin />
+            </a>
+        </footer>
+    )
+}
---
+++ React-Portfolio/src/components/Resume.jsx
@@ -0,0 +1,16 @@
+export default function Resume() {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <iframe src="" title="Link to Joel's Resume">Download my Resume here!</iframe>
+            <h3>Front-End Skills</h3>
+            <ul>
+
+            </ul>
+            <h3>Back-End Skills</h3>
+            <ul>
+                
+            </ul>
+        </section>
+    )
+}

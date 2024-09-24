deb7a7c718d57b1872c76ae779a9021df7edfdb2
dTop <jroney002@gmail.com>
2024-09-24 18:20:45 -0500

    adding placeholder structure to about and contact

---
+++ React-Portfolio/src/components/Contact.jsx
@@ -0,0 +1,16 @@
+export default function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <form>
+                <p>Name:</p>
+                <input/>
+                <p>Email:</p>
+                <input/>
+                <p>Message:</p>
+                <input/>
+            </form>
+            <button>Submit</button>
+        </div>
+    );
+}

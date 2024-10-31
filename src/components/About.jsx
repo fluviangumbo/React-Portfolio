025cc4587b3e9cdc1ded8e53e980f0e8d49b5003
joel <jroney002@gmail.com>
2024-10-30 20:01:25 -0500

    added bio info to about and messed with colors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ export default function About() {
         <div>
             <h2>About Me</h2>
             {/* <image /> */}
-            <p>Bio info</p>
-            <p>Personal Info</p>
+            <p>I'm Joel Roney, a junior developer with a background in business and software sales. I bring a unique perspective to development, combining technical skills with experience collaborating across departments and teams for the benefit of the whole organization. I'm driven by a passion for engineering efficiency and deepening my knowledge of the software landscape.</p>
+            <p>I’m rapidly expanding my expertise in full-stack development. My prior experience in business and software sales has given me social and team skills that drive value for users and companies alike. I have a particular fondness of databases and enjoy learning new tools and frameworks to keep pace with the evolving tech landscape. I focus on design patterns and am constantly looking for ways to program smarter. Outside of development, I enjoy exploring the world of music, and have a deep love for film.</p>
         </div>
     );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,14 +2,26 @@ export default function Resume() {
     return (
         <section className="resume" >
             <h2>Resume</h2>
-            <iframe src="" title="Link to Joel's Resume">Download my Resume here!</iframe>
             <h3>Front-End Skills</h3>
             <ul>
-
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>Responsive Design and SEO</li>
+                <li>React</li>
+                <li>Bootstrap</li>
             </ul>
             <h3>Back-End Skills</h3>
             <ul>
-                
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>PostgresQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+                <li>Testing (Vitest/Cypress)</li>
             </ul>
         </section>
     )

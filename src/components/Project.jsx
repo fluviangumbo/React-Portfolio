b6e7f2f578751af784750a2f655540887315f0eb
joel <jroney002@gmail.com>
2024-11-03 11:58:39 -0600

    added some images and started styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,37 +1,44 @@
 import ProjectCard from './ProjectCard';
 
 export default function Project() {
-    const projects = [
+    const projects = [ // NEED CLI APP SCREENSHOTS
         {
-            // image: './images/...',
+            image: './src/images/Weather-API-App.png',
             imageAlt: 'A weather forecast web application',
             title: 'Weather API Web Application',
             deployed: 'https://weather-api-app-uhqg.onrender.com/',
             repository: 'https://github.com/fluviangumbo/Weather-API-App',
         },
         {
-            // image: './images/WHATEVER_IMAGE', // Don't leave this
+            image: './src/images/Board-Game-Night.png',
             imageAlt: 'A web application for creating groups and organizing board game nights.',
             title: 'Boardgame Night Organizer',
             deployed: 'https://board-game-night.onrender.com',
             repository: 'https://github.com/fluviangumbo/Board-Game-Night',
         },
         {
-            // image: './images/...'
+            // image: './src/images/...',
             imageAlt: 'A CLI application for generating READMEs',
             title: 'CLI README.md Generator',
             deployed: '',
             repository: 'https://github.com/fluviangumbo/CLI-README-Generator',
         },
         {
-            // image: './images/...'
+            // image: './src/images/...',
             imageAlt: 'A CLI app for managing vehicles',
             title: 'TypeScript Vehicle CLI App',
             deployed: '',
             repository: 'https://github.com/fluviangumbo/TS-Vehicle-App',
         },
         {
-            // image: './images/...'
+            // image: './src/images/...',
+            imageAlt: 'A CLI app for managing employees and departments',
+            title: 'Employee CMS CLI App',
+            deployed: '',
+            repository: 'https://github.com/fluviangumbo/SQL-CLI-App',
+        },
+        {
+            image: './src/images/Mgmt-Calendar.png',
             imageAlt: 'A weekly organizer for managers/employers',
             title: 'Management Week Organizer',
             deployed: 'https://fluviangumbo.github.io/Management-Calendar/',
@@ -44,7 +51,7 @@ export default function Project() {
             <ul>
                 {
                     projects.map((project, i) => (
-                        <ProjectCard /*image={project.image} */ className="project-card" imageAlt={project.imageAlt} title={project.title} deployed={project.deployed} repository={project.repository} key={i} />
+                        <ProjectCard image={project.image} className="project-card" imageAlt={project.imageAlt} title={project.title} deployed={project.deployed} repository={project.repository} key={i} />
                     ))
                 }
             </ul>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 export default function ProjectCard(props) {
     return (
         <li>
-            {/* <img alt="{props.imageAlt}" src={props.image} /> */}
+            <img alt={props.imageAlt} src={props.image} />
             <p>{props.title}</p>
             <a href={props.deployed}>Deployed Application</a>
             <a href={props.repository}>GitHub Repository</a>

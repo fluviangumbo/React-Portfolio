a1cae391760e02e9fd91660562d84dd446e0c6fa
joel <jroney002@gmail.com>
2025-01-27 17:58:09 -0600

    added text to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer>
+            <p>Github Profile: </p>
             <a href="https://github.com/fluviangumbo">
                 <FaGithubSquare />
             </a>
+            <p>LinkedIn Profile: </p>
             <a href="https://www.linkedin.com/in/joel-roney-099983161/">
                 <FaLinkedin />
             </a>

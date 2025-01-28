ecd8eb7bad7ddff68101a037122d010d41223288
joel <jroney002@gmail.com>
2025-01-27 18:12:02 -0600

    added resume, fixed footer

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,11 @@
 export default function Resume() {
     return (
         <section className="resume" >
+            <div className="resume-link">
+                <a href="https://docs.google.com/document/d/1W8q_r5Q_HEGPfssyZ4NbSgia3fyX4UK0KOtYzU5i4nc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    Checkout My Resume!
+                </a>
+            </div>
             <div className="skills">
                 <div className="skill-category">
                     <h3>Front-End Skills</h3>

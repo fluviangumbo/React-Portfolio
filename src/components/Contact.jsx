3b401a0461d918c84c4b0d7c44312f190cf7df8a
joel <jroney002@gmail.com>
2025-01-27 18:37:23 -0600

    added resume to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,11 @@ export default function Contact() {
     return (
         <div className="contact">
             <h2>Contact Me!</h2>
+            <div className="resume-contact">
+                <a href="https://docs.google.com/document/d/1W8q_r5Q_HEGPfssyZ4NbSgia3fyX4UK0KOtYzU5i4nc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    See My Resume!
+                </a>
+            </div>
             <div>
                 <p>The best wat to reach me is by email:</p>
                 <h3>jroney002@gmail.com</h3>
